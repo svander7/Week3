e07e94765d482f6342bfa3a3262137f4d40f8fc8
svander7 <147276430+svander7@users.noreply.github.com>
2023-12-03 17:13:53 -0500

    Initial commit

---
+++ src/com/coderscampus/objects/RunnableClass.java
@@ -0,0 +1,16 @@
+package com.coderscampus.objects;
+
+public class RunnableClass {
+
+	public static void main(String[] args) {
+		// Class -> Blueprint for a Object
+		// Object -> A Noun, it's an instantiation of a Class
+		
+		// Data type, variable name, assignment operation
+		// int someNumber = 39;
+
+		Human trevorPage = new Human();
+		
+	}
+
+}
